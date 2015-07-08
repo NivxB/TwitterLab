8c7db05c9e6a736c79795d187a48567af104bb66
Kevin Barahona <kbarahona@localhost.localdomain>
2015-07-08 01:30:41 -0600

    Oh si

--- src/twitterlab/TwitterLab.java
+++ src/twitterlab/TwitterLab.java
@@ -6,6 +6,10 @@
 package twitterlab;
 
 import javax.security.auth.login.Configuration;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
@@ -19,11 +23,30 @@ public class TwitterLab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        
+
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setOAuthConsumerKey("yBSAPlE6wiAQU6CyIiXHSapwN");
         cb.setOAuthConsumerSecret("3CjbEkYyIZvNZ07KYpC55v9vpqxoNmMs1IkC48Wqr5tKXjUnZk");
+
+        try {
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            System.out.println("-----");
+
+            // get request token.
+            // this will throw IllegalStateException if access token is already available
+            // this is oob, desktop client version
+            RequestToken requestToken = twitter.getOAuthRequestToken();
+
+            System.out.println("Got request token.");
+            System.out.println("Request token: " + requestToken.getToken());
+            System.out.println("Request token secret: " + requestToken.getTokenSecret());
+
+            
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
     }
-    
+
 }
